46334d3f74c922001f10972764e754cecbc83e8a
Yasmina Gil <yasmina.gil@spendit.de>
2020-01-17 15:12:10 +0100

    all

--- src/test/java/Test/Android_Grid_Parallel.java
+++ src/test/java/Test/Android_Grid_Parallel.java
@@ -2,14 +2,20 @@ package Test;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -22,12 +28,13 @@ public class Android_Grid_Parallel {
     
     @BeforeClass
     @Parameters({"deviceName","UDID", "platformVersion","node_port"})
-    public void setUp(String deviceName,String UDID, String platformVersion, String node_port ) throws MalformedURLException {
+    public void setUp(String deviceName,String UDID, String platformVersion, String node_port ) throws IOException {
         
         File classpathRoot = new File(System.getProperty("user.dir"));
         File appDir = new File(classpathRoot, "/src/app/Android");
         File app = new File(appDir, "app-beta-debug.apk");
         System.out.println("app dir: "+app.getAbsolutePath());
+       
         capabilities.setCapability("app", app.getAbsolutePath());
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("udid", UDID);
@@ -35,7 +42,8 @@ public class Android_Grid_Parallel {
         capabilities.setCapability("deviceName", deviceName);
         URL url = new URL("http://localhost:" + node_port +"/wd/hub");
         androidDriver = new AndroidDriver(url, capabilities);
-       
+        File receiptDir = new File(classpathRoot, "/src/test/resources/receipts/DE_RECEIPT.jpg");
+        androidDriver.pushFile("/sdcard/DE_RECEIPT.jpg", Base64.encodeBase64(FileUtils.readFileToByteArray(receiptDir)));
         capabilities.setCapability("noReset", "true");
         capabilities.setCapability("uiautomator2ServerLaunchTimeout",40000);
         System.out.println("deviceName: "+deviceName);
@@ -47,8 +55,6 @@ public class Android_Grid_Parallel {
     public void testLunchit() throws InterruptedException {
         
         System.out.println("capabilities: "+capabilities.toString());
-    
-      
         MobileElement el1 = (MobileElement) androidDriver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout");
         el1.click();
         MobileElement el2 = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/aet_email");
@@ -59,8 +65,15 @@ public class Android_Grid_Parallel {
         el4.click();
         MobileElement close = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/close_happy");
         close.click();
-        Thread.sleep(10000);
+        WebDriverWait wait = new WebDriverWait(androidDriver, 10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/addReceipt"))).click();
+        Thread.sleep(1000);
+        MobileElement el6 = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/btn_close");
+        el6.click();
         
+       
+    
+    
     }
     @AfterClass
     public void tearDown() {
--- start-all-configs.sh
+++ start-all-configs.sh
@@ -3,7 +3,6 @@ appium --nodeconfig src/app/nodes-config/android-node1.json -p 1234 -cp 1234  &
 appium --nodeconfig src/app/nodes-config/android-node2.json -p 3456 -cp 3456 &
 appium --nodeconfig src/app/nodes-config/ios-node.json -p 4730 -cp 4730 &
 cd ~/Library/Android/sdk/tools/
-./emulator -avd "Nexus_5_API_22" &
 ./emulator -avd "Pixel_3_XL_API_29"
 
 
