055283bb86a74a145fe11684e0e50e7c2a0d31a0
Yasmina Gil <yasmina.gil@spendit.de>
2020-01-23 20:17:55 +0100

    first commit

--- src/test/java/Test/Android_Grid_Parallel.java
+++ src/test/java/Test/Android_Grid_Parallel.java
@@ -1,12 +1,17 @@
 package Test;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -16,72 +21,115 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Android_Grid_Parallel {
-    
-    public AndroidDriver androidDriver;
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-    
-    
-    @BeforeClass
-    @Parameters({"deviceName","UDID", "platformVersion","node_port"})
-    public void setUp(String deviceName,String UDID, String platformVersion, String node_port ) throws IOException {
-        
-        File classpathRoot = new File(System.getProperty("user.dir"));
-        File appDir = new File(classpathRoot, "/src/app/Android");
-        File app = new File(appDir, "app-beta-debug.apk");
-        System.out.println("app dir: "+app.getAbsolutePath());
-       
-        capabilities.setCapability("app", app.getAbsolutePath());
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("udid", UDID);
-        capabilities.setCapability("platformVersion", platformVersion);
-        capabilities.setCapability("deviceName", deviceName);
-        URL url = new URL("http://localhost:" + node_port +"/wd/hub");
-        androidDriver = new AndroidDriver(url, capabilities);
-        File receiptDir = new File(classpathRoot, "/src/test/resources/receipts/DE_RECEIPT.jpg");
-        androidDriver.pushFile("/sdcard/DE_RECEIPT.jpg", Base64.encodeBase64(FileUtils.readFileToByteArray(receiptDir)));
-        capabilities.setCapability("noReset", "true");
-        capabilities.setCapability("uiautomator2ServerLaunchTimeout",40000);
-        System.out.println("deviceName: "+deviceName);
-        System.out.println("udid: "+UDID);
-        androidDriver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void testLunchit() throws InterruptedException {
-        
-        System.out.println("capabilities: "+capabilities.toString());
-        MobileElement el1 = (MobileElement) androidDriver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout");
-        el1.click();
-        MobileElement el2 = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/aet_email");
-        el2.sendKeys("happyspendit.de.test1@gmail.com");
-        MobileElement el3 = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/aet_password");
-        el3.sendKeys("lindaBrown124!");
-        MobileElement el4 = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/btn_login");
-        el4.click();
-        MobileElement close = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/close_happy");
-        close.click();
-        WebDriverWait wait = new WebDriverWait(androidDriver, 10);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/addReceipt"))).click();
-        Thread.sleep(1000);
-        MobileElement el6 = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/btn_close");
-        el6.click();
-        
-       
-    
-    
-    }
-    @AfterClass
-    public void tearDown() {
-        /*if (androidDriver!=null){
-            androidDriver.quit();
-        }*/
-    }
+	
+	public AndroidDriver androidDriver;
+	DesiredCapabilities capabilities = new DesiredCapabilities();
+	
+	
+	@BeforeClass
+	@Parameters({"deviceName", "UDID", "platformVersion", "node_port"})
+	public void setUp(String deviceName, String UDID, String platformVersion, String node_port) throws IOException {
+		
+		File classpathRoot = new File(System.getProperty("user.dir"));
+		File appDir = new File(classpathRoot, "/src/app/Android");
+		File app = new File(appDir, "app-beta-debug.apk");
+		System.out.println("app dir: " + app.getAbsolutePath());
+		
+		capabilities.setCapability("app", app.getAbsolutePath());
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("udid", UDID);
+		capabilities.setCapability("platformVersion", platformVersion);
+		capabilities.setCapability("deviceName", deviceName);
+		capabilities.setCapability("autoGrantPermissions", true);
+		capabilities.setCapability("fullReset", "true");
+		capabilities.setCapability("noReset", "false");
+		//capabilities.setCapability("unicodeKeyboard", true);
+		capabilities.setCapability("resetKeyboard", true);
+		// capabilities.setCapability("uiautomator2ServerLaunchTimeout",40000);
+		capabilities.setCapability("printPageSourceOnFindFailure", "true");
+	
+		URL url = new URL("http://localhost:" + node_port + "/wd/hub");
+		androidDriver = new AndroidDriver(url, capabilities);
 
-   
-
+		System.out.println("deviceName: " + deviceName);
+		System.out.println("udid: " + UDID);
+		System.out.println("session: " + androidDriver.getSessionId());
+		
+		
+		androidDriver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void testLunchit() throws InterruptedException {
+		File classpathRoot = new File(System.getProperty("user.dir"));
+		System.out.println("settings: " + androidDriver.getCapabilities());
+		MobileElement el1 = (MobileElement) androidDriver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout");
+		el1.click();
+		//androidDriver.hideKeyboard();
+		
+		MobileElement el2 = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/aet_email");
+		
+		el2.sendKeys("happyspendit.de.test1@gmail.com");
+		MobileElement el3 = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/aet_password");
+		el3.sendKeys("lindaBrown124!");
+		MobileElement el4 = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/btn_login");
+		el4.click();
+		MobileElement close = (MobileElement) androidDriver.findElementById("com.lunchit.android.beta:id/close_happy");
+		close.click();
+		WebDriverWait wait = new WebDriverWait(androidDriver, 10);
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/addReceipt"))).click();
+		Thread.sleep(1000);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/btn_close"))).click();
+		Thread.sleep(2000);
+		File receiptDir1 = new File(classpathRoot, "/src/test/resources/receipts/DE_RECEIPT1.jpg");
+		try {
+			androidDriver.pushFile("/sdcard/Pictures/DE_RECEIPT1.jpg", Base64.encodeBase64(FileUtils.readFileToByteArray(receiptDir1)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]"))).click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.android.documentsui:id/icon_thumb"))).click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/btnVerify"))).click();
+		//Thread.sleep(1000);
+		//wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/amountValue"))).click();
+		//androidDriver.hideKeyboard();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/amountValue"))).click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/amountValue"))).clear();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/amountValue"))).sendKeys("300");
+		//	androidDriver.getKeyboard().sendKeys(Keys.ENTER);
+		Thread.sleep(1000);
+		WebDriverWait waitConditions = new WebDriverWait(androidDriver, 20);
+		//androidDriver.hideKeyboard();
+		waitConditions.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/legalSwitch"))).click();
+		waitConditions.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/submitButton"))).click();
+		Thread.sleep(1000);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/done_button"))).click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/closeFeedback"))).click();
+		Thread.sleep(1000);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/receiptsContainer"))).click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]"))).click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.LinearLayout"))).click();
+		
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/deleteButton"))).click();
+		Thread.sleep(1000);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/removeReceipt"))).click();
+		Thread.sleep(1000);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.lunchit.android.beta:id/done_button"))).click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]"))).click();
+		androidDriver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"Logout\").instance(0))").click();
+		
+	}
+	@AfterClass
+	public void tearDown() {
+		if (androidDriver!=null){
+			androidDriver.quit();
+		}
+	}
+	
 }
--- start-all-configs.sh
+++ start-all-configs.sh
@@ -2,8 +2,8 @@ java -jar src/test/drivers/selenium-server-standalone-3.11.0.jar -role hub -host
 appium --nodeconfig src/app/nodes-config/android-node1.json -p 1234 -cp 1234  &
 appium --nodeconfig src/app/nodes-config/android-node2.json -p 3456 -cp 3456 &
 appium --nodeconfig src/app/nodes-config/ios-node.json -p 4730 -cp 4730 &
-cd ~/Library/Android/sdk/tools/
-./emulator -avd "Pixel_3_XL_API_29"
+~/Library/Android/sdk/emulator/emulator -avd "Pixel_3_XL_API_29"
+~/Library/Android/sdk/emulator/emulator -avd "Nexus_5X_API_29"
 
 
 
