96477113faac46c5c6d707328bda1c1198083753
Yasmina Gil <yasmina.gil@spendit.de>
2020-01-25 23:17:02 +0100

    adding iphone 11 simulator launch in script

--- start-all-configs.sh
+++ start-all-configs.sh
@@ -4,7 +4,7 @@ appium --nodeconfig src/app/nodes-config/android-node2.json -p 3456 -cp 3456 &
 appium --nodeconfig src/app/nodes-config/ios-node.json -p 4730 -cp 4730 &
 ~/Library/Android/sdk/emulator/emulator -avd "Pixel_3_XL_API_29"
 ~/Library/Android/sdk/emulator/emulator -avd "Nexus_5X_API_29"
-
+open -a Simulator --args -CurrentDeviceUDID D4A14667-1951-4D09-88B0-B61E490C0311
 
 
 
