00b9b81d5b899eedcb67c689b93cf53c36313cc0
Yasmina Gil <yasmina.gil@spendit.de>
2020-02-12 14:59:57 +0100

    mo

--- src/test/java/screens/interfaces/LoginScreen.java
+++ src/test/java/screens/interfaces/LoginScreen.java
@@ -1,8 +1,5 @@
 package screens.interfaces;
 
-import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.By;
-
 public interface LoginScreen extends CommonScreen{
 	
 	public void enterLogin(String login) ;
